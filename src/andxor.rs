b9fcba7c514ca9dddd33641fff06b256051601b5
Yohei Tamura <tamuhey@gmail.com>
2020-09-19 19:31:14 +0900

    add andxor: and xor rig

---
+++ src/andxor.rs
@@ -0,0 +1,72 @@
+use num::{One, Zero};
+use std::ops::{Add, BitAnd, BitXor, Mul};
+// And Xor rig
+#[derive(Debug, Copy, Clone)]
+struct AX<T>(pub T);
+
+impl<T> Add<AX<T>> for AX<T>
+where
+    T: BitXor<Output = T> + Copy,
+{
+    type Output = AX<T::Output>;
+    fn add(self, rhs: Self) -> Self::Output {
+        Self(self.0 ^ rhs.0)
+    }
+}
+impl<T> Mul<AX<T>> for AX<T>
+where
+    T: BitAnd<Output = T> + Copy,
+{
+    type Output = AX<T::Output>;
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self(self.0 & rhs.0)
+    }
+}
+impl<T> From<T> for AX<T> {
+    fn from(x: T) -> Self {
+        Self(x)
+    }
+}
+use std::str::FromStr;
+impl<T> FromStr for AX<T>
+where
+    T: FromStr,
+{
+    type Err = T::Err;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let v = T::from_str(s)?;
+        Ok(Self(v))
+    }
+}
+
+use std::fmt::Display;
+impl<T> Display for AX<T>
+where
+    T: Display,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl<T> Zero for AX<T>
+where
+    T: Zero + Copy + PartialEq + BitXor,
+    Self: Add<Output = Self>,
+{
+    fn zero() -> Self {
+        Self(T::zero())
+    }
+    fn is_zero(&self) -> bool {
+        self.0.is_zero()
+    }
+}
+
+impl One for AX<usize> {
+    fn one() -> Self {
+        Self(!0)
+    }
+    fn is_one(&self) -> bool {
+        self.0.is_one()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ extern crate quickcheck;
 #[cfg(test)]
 #[macro_use(quickcheck)]
 extern crate quickcheck_macros;
-use std::str::FromStr;
+pub mod andxor;
 pub mod bellman_ford;
 pub mod binary_search;
 pub mod bit;
